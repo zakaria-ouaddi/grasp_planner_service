97cbaae5e2d60e5254233d98731eae61d7f80c40
zakaria ouaddi <64169246+zakaria-ouaddi@users.noreply.github.com>
2025-12-30 23:09:01 +0100

    Delete install directory

--- install/grasp_planner_msgs/share/grasp_planner_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(grasp_planner_msgs_IDL_FILES "srv/PlanGrasp.idl")
-set(grasp_planner_msgs_INTERFACE_FILES "srv/PlanGrasp.srv")
